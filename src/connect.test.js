c7098d518d994d38eba8be37f319cd0f425f93a6
Daigo UCHIYAMA <daigo.uchiyama@gmail.com>
2017-02-21 08:42:55 +0900

    Add tests

--- src/connect.test.js
+++ src/connect.test.js
@@ -4,19 +4,64 @@ import { Flux } from 'flumpt';
 import { render } from 'enzyme';
 import connect from './connect';
 
-test('Connected component', (t) => {
+/* eslint-disable react/prop-types */
+
+test('Connected component has modified properties', (t) => {
+  class OriginalClass extends React.Component {
+    render() {
+      return <div />;
+    }
+  }
+  const ConnectedClass = connect()(OriginalClass);
+
+  t.is(ConnectedClass.WrappedComponent, OriginalClass);
+  t.is(ConnectedClass.displayName, 'Connect(OriginalClass)');
+
+  const OriginalFunc = () => <div />;
+  const ConnectedFunc = connect()(OriginalFunc);
+
+  t.is(ConnectedFunc.WrappedComponent, OriginalFunc);
+  t.is(ConnectedFunc.displayName, 'Connect(OriginalFunc)');
+});
+
+test('Connected component receives props modified by mapStateToProps', (t) => {
+  let rendered = false;
+
   const mapStateToProps = ({ passed, omitted }) => ({ passed }); // eslint-disable-line no-unused-vars
-  const Original = ({ passed, omitted, dispatch }) => { // eslint-disable-line react/prop-types
+  const Original = ({ passed, omitted }) => {
+    rendered = true;
     t.is(passed, 'passed');
     t.falsy(omitted);
-    t.is(typeof dispatch, 'function');
-    dispatch('call');
     return <div />;
   };
   const Connected = connect(mapStateToProps)(Original);
 
-  t.is(Connected.WrappedComponent, Original);
-  t.is(Connected.displayName, 'Connect(Original)');
+  class TestApp extends Flux {
+    render() {
+      return <Connected />;
+    }
+  }
+
+  const app = new TestApp({
+    renderer: (el) => render(el),
+    initialState: {
+      passed: 'passed',
+      omitted: 'omitted',
+    },
+  });
+
+  app.update((state) => state);
+
+  t.true(rendered);
+});
+
+test('Connected component can call dispatch', (t) => {
+  const Original = ({ dispatch }) => {
+    t.is(typeof dispatch, 'function');
+    dispatch('call');
+    return <div />;
+  };
+  const Connected = connect()(Original);
 
   let called = false;
 
@@ -33,12 +78,69 @@ test('Connected component', (t) => {
 
   const app = new TestApp({
     renderer: (el) => render(el),
-    initialState: {
-      passed: 'passed',
-      omitted: 'omitted',
-    },
+    initialState: {},
   });
+
   app.update((state) => state);
 
   t.true(called);
+});
+
+test('Connected component receives state as it is if no mapStateToProps is given', (t) => {
+  const givenState = { a: 1, b: 2, c: 3 };
+  let rendered = false;
+
+  const Original = (props) => {
+    rendered = true;
+    t.is(typeof props.dispatch, 'function');
+    const expected = Object.assign({}, givenState, { dispatch: props.dispatch });
+    t.deepEqual(props, expected);
+    return <div />;
+  };
+  const Connected = connect()(Original);
+
+  class TestApp extends Flux {
+    render() {
+      return <Connected />;
+    }
+  }
+
+  const app = new TestApp({
+    renderer: (el) => render(el),
+    initialState: givenState,
+  });
+
+  app.update((state) => state);
+
+  t.true(rendered);
+});
+
+test('Connected component receives merged props', (t) => {
+  const givenProps = { a: 'props', b: 'props', dispatch: 'props' };
+  const givenState = { b: 'state', dispatch: 'state' };
+  let rendered = false;
+
+  const Original = ({ a, b, dispatch }) => {
+    rendered = true;
+    t.is(a, 'props');
+    t.is(b, 'state');
+    t.is(typeof dispatch, 'function');
+    return <div />;
+  };
+  const Connected = connect()(Original);
+
+  class TestApp extends Flux {
+    render() {
+      return <Connected { ...givenProps } />;
+    }
+  }
+
+  const app = new TestApp({
+    renderer: (el) => render(el),
+    initialState: givenState,
+  });
+
+  app.update((state) => state);
+
+  t.true(rendered);
 });
